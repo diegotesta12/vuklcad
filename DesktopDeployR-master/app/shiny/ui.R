f6fe5caa667670781ba2ef15a19345cb003e02c0
diegotesta12 <83613571+diegotesta12@users.noreply.github.com>
2022-02-21 10:06:35 -0300

    Add files via upload

---
+++ DesktopDeployR-master/app/shiny/ui.R
@@ -0,0 +1,29 @@
+#' This is an example shiny application
+#' It is the same as example_02 in the RStudio Shiny Tutorial
+
+library(shiny)
+
+ui = fluidPage(
+  titlePanel("Example Shiny App"),
+  sidebarLayout(
+
+    sidebarPanel(
+      selectInput(
+        inputId = "dataset",
+        label = "Choose a dataset",
+        choices = c("rock", "pressure", "cars", "iris")),
+
+      numericInput(
+        inputId = "obs",
+        label = "Number of observations to view:",
+        value = 10)
+    ),
+
+    mainPanel(
+      tags$div(sprintf("Global Variable Value: %s", GLOBAL_VAR)),
+      verbatimTextOutput("commandArgs"),
+      verbatimTextOutput("dataSummary"),
+      tableOutput("dataView")
+    )
+  )
+)
